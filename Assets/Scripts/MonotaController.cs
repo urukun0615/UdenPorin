acc9640333c4642bc209fca13f9474b2325ac1e3
k-ayumu-6 <111178685+k-ayumu-6@users.noreply.github.com>
2024-07-23 14:09:22 +0900

    左右移動を追加

--- Assets/Scripts/MonotaController.cs
+++ Assets/Scripts/MonotaController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class MonotaController : MonoBehaviour
 {
+    public float speed = 10.0f;
+    public Camera playerCamera;
+    bool isGround = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +16,30 @@ public class MonotaController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        //ADÉLÅ[éÊìæ
+        float horizontal = Input.GetAxis("Horizontal");
+
+        // Get the direction the camera is facing
+        Vector3 right = playerCamera.transform.right;
+
+        right.y = 0f;
+
+        right.Normalize();
+
+        // Calculate the direction based on the input
+        Vector3 direction = right * horizontal;
+
+        // Move the player
+        transform.Translate(direction * speed * Time.deltaTime, Space.World);
+
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            Junp();
+        }
+    }
+
+    public void Junp()
     {
         
     }
